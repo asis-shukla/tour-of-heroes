a14586eafa70d5f904cfa1c930ba6a24097063f2
Ashish Shukla <ashish_shukla@epam.com>
2023-12-28 15:06:50 +0530

    adde heros list and hero edit

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, UpperCasePipe } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heros';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, UpperCasePipe, FormsModule],
+  imports: [CommonModule, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
